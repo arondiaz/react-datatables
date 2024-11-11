bfb14987e8a27d19a506c8b630e956b17d438a20
Arón Díaz <arondiaz.cpn@gmail.com>
2024-11-11 17:54:00 -0300

    implemented basic data tables library

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import DataTable from "react-data-table-component";
 
-function App() {
-  const [count, setCount] = useState(0)
+const data = [
+  {
+    id: 1,
+    name: "Pepe",
+    age: 23,
+  },
+  {
+    id: 2,
+    name: "Pae",
+    age: 42,
+  },
+  {
+    id: 3,
+    name: "Mark",
+    age: 34,
+  },
+];
 
+const columns = [
+  {
+    name: "ID",
+    selector: (row) => row.id,
+  },
+  {
+    name: "NOMBRE",
+    selector: (row) => row.name,
+  },
+  {
+    name: "EDAD",
+    selector: (row) => row.age,
+  },
+];
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
+};
 
-export default App
+export default App;
